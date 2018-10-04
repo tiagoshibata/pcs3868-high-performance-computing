82361628e7c5871d2ab2ca5b1f7c0c4c869d77d8
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-10-04 09:49:40 -0300

    Add parallel matrix element search

---
+++ 05-openmp/find_element.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/sysinfo.h>
+#include <time.h>
+
+#include <omp.h>
+
+#define SIZE 1000
+double A[SIZE][SIZE];
+
+int main() {
+    int nprocs = get_nprocs();
+    printf("nprocs = %d\n", nprocs);
+    omp_set_num_threads(nprocs);
+
+    #pragma omp parallel for
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            A[i][j] = (2 * i - j) % 1000;
+        }
+    }
+
+    for (;;) {
+        float target;
+        printf("Type target element:\n");
+        scanf("%f", &target);
+
+        struct timespec start_time, end_time;
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+        double occurences = 0;
+        #pragma omp parallel for reduction(+:occurences)
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                occurences += A[i][j] == target;
+            }
+        }
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        printf("Occurences: %f, time: %.5fms\n", occurences,
+            (end_time.tv_sec * 1000 + 1e-6 * end_time.tv_nsec) - (start_time.tv_sec * 1000 + 1e-6 * start_time.tv_nsec));
+    }
+
+    return 0;
+}
