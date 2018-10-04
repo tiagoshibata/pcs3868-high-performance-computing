7052414a4a35da6057c8f03b4e37383e72dd2737
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-10-04 09:37:22 -0300

    Optimize team creation in sum of matrix elements

--- 05-openmp/sum.c
+++ 05-openmp/sum.c
@@ -14,20 +14,26 @@ int main() {
 
     struct timespec start_time, end_time;
     clock_gettime(CLOCK_MONOTONIC, &start_time);
-    #pragma omp parallel for
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            A[i][j] = (3 * i - j) / 2;
+    double sum = 0;
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                A[i][j] = (3 * i - j) / 2;
+            }
+        }
+
+        #pragma omp barrier
+
+        #pragma omp for reduction(+:sum)
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                sum += A[i][j];
+            }
         }
     }
 
-    double sum = 0;
-    #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            sum += A[i][j];
-        }
-    }
     clock_gettime(CLOCK_MONOTONIC, &end_time);
     printf("Sum: %f, time: %.5fms\n", sum,
         (end_time.tv_sec * 1000 + 1e-6 * end_time.tv_nsec) - (start_time.tv_sec * 1000 + 1e-6 * start_time.tv_nsec));
