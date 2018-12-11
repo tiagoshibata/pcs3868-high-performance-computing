fdf66a5f1db5831ae9e6d14342e6cf7ce5265d36
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-12-11 18:43:31 -0200

    Fix OMP compile options to match serial program

--- capstone/src/simplex_noise_serial.c
+++ capstone/src/simplex_noise_serial.c
@@ -4,15 +4,12 @@
 #include "simplex_noise.h"
 
 int main() {
-    // for (float x = 0; x < 10; x += 0.05) {
-    //     printf("%f\n", simplex_noise_1d(x));
-    // }
     struct timespec start_time, end_time;
     clock_gettime(CLOCK_MONOTONIC, &start_time);
 
-    for (int x = 0; x < 400; x++) {
-        printf("%f %f\n", 0.05*x, simplex_noise_fractal_1d(12, 0.05*x));
-        fflush(stdout);
+    for (int x = 0; x < 400000; x++) {
+        simplex_noise_fractal_1d(16, 0.05*x);
+        // printf("%f %f\n", 0.05*x, simplex_noise_fractal_1d(12, 0.05*x));
     }
 
     clock_gettime(CLOCK_MONOTONIC, &end_time);
